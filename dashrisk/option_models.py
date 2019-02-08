c1ca776fea89cd8e0073a5f35657e2ee89660164
henry perlman <bperlman1@gmail.com>
2019-02-08 10:41:48 -0500

    option_models.py add model from symbol

--- dashrisk/option_models.py
+++ dashrisk/option_models.py
@@ -12,14 +12,18 @@ from scipy import stats as ss
 import copy
 import datetime
 import pytz
-from future.backports.datetime import tzinfo
 
 class BlackScholesBasic(object):
+    '''
+    Basic math to implement the Black Scholes options model
+    '''
     def __init__(self, *args, **kwargs):
         object.__init__(self, *args, **kwargs)
         
     #Black and Scholes
     def d1(self,S0, K, r, sigma, T):
+        if (sigma * np.sqrt(T)) ==0:
+            print('zero')
         return (np.log(S0/K) + (r + sigma**2 / 2) * T)/(sigma * np.sqrt(T))
      
     def d2(self,S0, K, r, sigma, T):
@@ -39,6 +43,9 @@ class BlackScholesBasic(object):
 
 
 class BaseModel():
+    '''
+    Base Options model that will calculate an options price and greeks
+    '''
     REFERENCE_VOL = .2
     ONE_DAY_SCAL = (1/365.0)**.5
     ATM_PERCENT_CHANGE_PER_VOL = (.001/(REFERENCE_VOL))
@@ -47,6 +54,18 @@ class BaseModel():
     DEFAULT_TIMEZONE = pytz.timezone('US/Eastern')
     
     def __init__(self,expiry_datetime,strike,c_p,atm_price,input_vol,rate,carry=None,time_zone=None):
+        '''
+        
+        :param expiry_datetime:  a python dateTime object like datetime.datetime(2019,5,22) for the March expiry of AAPL
+        :param strike: any float number that represents a strike price
+        :param c_p: one of "c" for call or "p" for put.  (case insensitive)
+        :param atm_price: current underlying "at the money" price
+        :param input_vol: annualized standard deviation of underlying
+        :param rate: interest rate for the term between now and expiry
+        :param carry: carry interest rate for the term between now and expiry
+        :param time_zone: a pytz timezone. Default: pytz.timezine('US/Eastern')
+        '''
+        self.expiry_datetime = expiry_datetime
         self.strike = strike
         self.c_p = c_p
         self.atm_price = atm_price
@@ -55,7 +74,7 @@ class BaseModel():
         self.carry=carry
         self.time_zone= BaseModel.DEFAULT_TIMEZONE if time_zone is None else time_zone
         dt_now = datetime.datetime.now().replace(tzinfo=BaseModel.DEFAULT_TIMEZONE)
-        days = (dt_now- expiry_datetime).days
+        days = (expiry_datetime - dt_now).days
         if days < 1:
             days = 1
         self.dte = days/365.0   
@@ -145,6 +164,89 @@ class BsModel(BaseModel):
         return bs.delta(self.c_p, self.atm_price, self.strike, self.rate, self.input_vol, self.dte)
 
 
+def model_from_symbol(full_symbol,atm_price,vol=None,rate=None,carry=None,model=BaseModel,tzinfo = None):
+    '''
+    extract information for BaseModel constructor from a string like 'SPY_20190322_250_c'
+    :param full_symbol: 
+        examples:                    
+            SPY_20190322_250_p   
+                symbol=SPY, strike=250.0, c_p=p year=2019, month=03, day=22, hour=17, minute=00, atm=atm_price, vol=0.18, rate=0.03, carry=0.0
+
+            SPY_201903221230_250_c  
+                symbol=SPY strike=250.0, c_p=c, year=2019, month=03, day=22, hour=12, minute=30, atm=atm_price, vol=0.18, rate=0.03, carry=0.0
+
+            SPY_20190322_250 
+                symbol=SPY strike=250.0, c_p=c, year=2019, month=03, day=22, hour=17, minute=00, atm=atm_price, vol=0.18, rate=0.03, carry=0.0
+
+            SPY_20190322_250_c_v.32_r.05_c.01 to specify any of  vol, rate or carry
+                symbol=SPY strike=250.0, c_p=c, year=2019, month=03, day=22, hour=17, minute=00, atm=atm_price, vol=0.32, rate=0.05, carry=0.01                
+
+            SPY  for the underlying, which is call option with a strike of .00001
+                If today is March 20th 2019, then:
+                symbol=SPY strike=.0001, c_p=p year=2019, month=03, day=21, hour=17, minute=00, atm=atm_price, vol=0.18, rate=0.03, carry=0.0
+                
+
+    :param tzinfo:
+    '''
+    tz = tzinfo if tzinfo is not None else BaseModel.DEFAULT_TIMEZONE
+    parts = full_symbol.split('_')
+    lp = len(parts)
+    expiry_date = datetime.datetime.now().replace(tzinfo=tz) + datetime.timedelta(5)
+    if lp>1:
+        year = int(parts[1][0:4])
+        mon = int(parts[1][4:6])
+        day = int(parts[1][6:8])
+        hour = 17 if len(parts[1])<10 else int(parts[1][8:10])
+        minute = 0 if len(parts[1])<10 else int(parts[1][10:12])
+        expiry_date = datetime.datetime(year,mon,day,hour,minute,tzinfo=tz)
+    strike  = .00001
+    if lp>2:
+        strike = float(str(parts[2]))
+    c_p  = 'c'
+    if lp > 3:
+        c_p = 'c' if lp < 3 else parts[3]
+    
+    param_dict = {
+        'v':.18 if vol is None else vol,
+        'r':.03 if rate is None else rate,
+        'c':0 if carry is None else carry,
+        }
+    for i in range(4,8):
+        if lp>i:
+            key = parts[i][0]
+            value = parts[i][1:]
+            param_dict[key] = value
+    vol = float(param_dict['v'])
+    r = float(param_dict['r'])
+    carry = float(param_dict['c'])
+    
+    op = model(expiry_date,strike,c_p,atm_price,vol,r,carry)
+    return op
+
+def symbol_from_model(symbol,model=BaseModel):
+    atm = model.atm_price
+    dt = model.expiry_datetime
+    year = dt.year
+    mon = dt.month 
+    day = dt.day 
+    hour = dt.hour 
+    minute = dt.minute 
+    yyyymmddhhmm = str('%04d%02d%02d%02d%02d' %(year,mon,day,hour,minute))
+    strike = str(model.strike)
+    c_p = model.c_p 
+    vol = str(model.input_vol)
+    r = str(model.rate)
+    c = str(model.carry)
+    return f'{symbol}_{yyyymmddhhmm}_{strike}_{c_p}_v{vol}_r{r}_c{c}'
+
+def yyyymmddhhmm_from_datetime(dt): 
+    year = dt.year
+    mon = dt.month 
+    day = dt.day 
+    hour = dt.hour 
+    minute = dt.minute 
+    yyyymmddhhmm = str('%04d%02d%02d%02d%02d' %(year,mon,day,hour,minute))
+    return yyyymmddhhmm
 
 if __name__=='__main__':
     pc = 'c'
@@ -153,7 +255,7 @@ if __name__=='__main__':
     vol = .2
     r = .03
     car = 0
-    expiry_date = datetime.datetime.now().replace(tzinfo=BaseModel.DEFAULT_TIMEZONE) - datetime.timedelta(30)
+    expiry_date = datetime.datetime.now().replace(tzinfo=BaseModel.DEFAULT_TIMEZONE) + datetime.timedelta(30)
     m = BaseModel(expiry_date,s,pc,k,vol,r,car)
     p = m.get_option_price()
     d = m.get_delta()
@@ -161,14 +263,44 @@ if __name__=='__main__':
     v = m.get_vega()
     t = m.get_theta()
     rh = m.get_rho()
-    print(p,d,g,v,t,rh)
+    print('basic model call price',p,d,g,v,t,rh)
     
     m = BaseModel(expiry_date,s,pc,k,vol,r-.01,car)
     p = m.get_option_price()
-    print(p)
+    print('price with rate - .01',p)
     m = BaseModel(expiry_date,s,pc,k,vol,r+.01,car)
     p = m.get_option_price()
-    print(p)
+    print('price with rate + .01',p)
+    
+    sym = f'TEST_{yyyymmddhhmm_from_datetime(expiry_date)}_{k}_{pc}_v{vol}_r{r}_c{car}'
+    m = model_from_symbol(sym,100)
+    p = m.get_option_price()
+    d = m.get_delta()
+    g = m.get_gamma()
+    v = m.get_vega()
+    t = m.get_theta()
+    rh = m.get_rho()
+    print(f'basic model call price using model_from_symbol({sym})',p,d,g,v,t,rh)
+    
+    sym = f'TEST_{yyyymmddhhmm_from_datetime(expiry_date)}'
+    m = model_from_symbol(sym,100)
+    p = m.get_option_price()
+    d = m.get_delta()
+    g = m.get_gamma()
+    v = m.get_vega()
+    t = m.get_theta()
+    rh = m.get_rho()
+    print(f'basic model call price using model_from_symbol({sym})',p,d,g,v,t,rh)
+    
+    sym = 'TEST'
+    m = model_from_symbol(sym,100)
+    p = m.get_option_price()
+    d = m.get_delta()
+    g = m.get_gamma()
+    v = m.get_vega()
+    t = m.get_theta()
+    rh = m.get_rho()
+    print(f'basic model call price using model_from_symbol({sym})',p,d,g,v,t,rh)
     
     
         
