abd0058fb8f3d6d92d48de1ba7c1038ed8f1b8f4
henry perlman <bperlman1@gmail.com>
2019-05-16 06:32:56 -0400

    changes to schedule_db_updates.py

--- dashrisk/schedule_db_updates.py
+++ dashrisk/schedule_db_updates.py
@@ -26,6 +26,9 @@ if __name__ == '__main__':
     parser.add_argument('--password',type=str,
                     help='password (None will be blank)',
                     nargs='?')
+    parser.add_argument('--days_to_fetch',type=int,
+                    help='days to fetch for the update.  These days will first be deleted before the update (default = 4)',
+                    default=4)
     args = parser.parse_args()
     h = args.hour
     logger = schedule_it.init_root_logger("logfile.log", "INFO")
@@ -34,7 +37,7 @@ if __name__ == '__main__':
         sch = schedule_it.ScheduleNext('hour', h,logger = logger)
         sch.wait()
         logger.info(f"updating history")
-        bh = build_history.HistoryBuilder(update_table=True,username=args.username,password=args.password,logger=logger)
+        bh = build_history.HistoryBuilder(update_table=True,username=args.username,password=args.password,logger=logger,days_to_fetch=args.days_to_fetch)
         bh.execute()
 #         bh.update_yahoo_daily()
         logger.info(f"sleeping for an hour before next scheduling")
